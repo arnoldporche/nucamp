1e61540ab55e8c5148e56f069de8b0d011808834
Administrator <administrator@Administrators-MacBook-Pro.local>
2020-01-31 11:57:59 -0800

    Fetch from Server

--- 3-React/nucampsite/src/components/MainComponent.js
+++ 3-React/nucampsite/src/components/MainComponent.js
@@ -12,13 +12,15 @@ import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
-import { addComment, fetchCampsites } from '../redux/ActionCreators';
+import { addComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 
 const mapDispatchToProps = {
   addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
   fetchCampsites: () => (fetchCampsites()),
-  resetFeedbackForm: () => (actions.reset('feedbackForm'))
+  resetFeedbackForm: () => (actions.reset('feedbackForm')),
+  fetchComments: () => (fetchComments()),
+  fetchPromotions: () => (fetchPromotions())
 };
 
 const mapStateToProps = state => {
@@ -43,30 +45,35 @@ class Main extends Component {
 
   componentDidMount() {
       this.props.fetchCampsites();
+      this.props.fetchComments();
+      this.props.fetchPromotions();
   }
 
   render() {
     const HomePage = () => {
         return (
-            <Home
-                campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
-                campsitesLoading={this.props.campsites.isLoading}
-                campsitesErrMess={this.props.campsites.errMess}
-                promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
-                partner={this.props.partners.filter(partner => partner.featured)[0]}
-            />
+          <Home
+              campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+              campsitesLoading={this.props.campsites.isLoading}
+              campsitesErrMess={this.props.campsites.errMess}
+              partner={this.props.partners.filter(partner => partner.featured)[0]}
+              promotion={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]}
+              promotionLoading={this.props.promotions.isLoading}
+              promotionErrMess={this.props.promotions.errMess}
+          />
         );
     }
 
     const CampsiteWithId = ({match}) => {
         return (
-            <CampsiteInfo 
-                campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                isLoading={this.props.campsites.isLoading}
-                errMess={this.props.campsites.errMess}
-                comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-                addComment={this.props.addComment}
-            />
+          <CampsiteInfo 
+              campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+              isLoading={this.props.campsites.isLoading}
+              errMess={this.props.campsites.errMess}
+              comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+              commentsErrMess={this.props.comments.errMess}
+              addComment={this.props.addComment}
+          />
         );
     };
 
