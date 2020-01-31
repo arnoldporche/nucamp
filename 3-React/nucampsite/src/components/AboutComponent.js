0e6a0f194b3f8f0980037142872cdc35a36aeeb1
Administrator <administrator@Administrators-MacBook-Pro.local>
2020-01-31 12:49:59 -0800

    Week 5 Workshop Assignment

--- 3-React/nucampsite/src/components/AboutComponent.js
+++ 3-React/nucampsite/src/components/AboutComponent.js
@@ -7,14 +7,47 @@ import {
   CardHeader,
   Media
 } from "reactstrap";
+import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
+import { Fade, Stagger } from "react-animation-components";
 import { Link } from "react-router-dom";
+
+function PartnerList(props) {
+  const partners = props.partners.partners.map(partner => {
+    return (
+      <Fade in key={partner.id}>
+        <Media tag="li">
+          <RenderPartner partner={partner} />
+        </Media>
+      </Fade>
+    );
+  });
+
+  if (props.partners.isLoading) {
+    return <Loading />;
+  }
+  if (props.partners.errMess) {
+    return (
+      <div className="col">
+        <h4>{props.partners.errMess}</h4>
+      </div>
+    );
+  }
+  return (
+    <div className="col mt-4">
+      <Media list>
+        <Stagger in>{partners}</Stagger>
+      </Media>
+    </div>
+  );
+}
 
 function RenderPartner({ partner }) {
   if (partner) {
     return (
       <React.Fragment>
-        <Media object src={partner.image} alt={partner.name} width="150" />
-        <Media body="true">
+        <Media object src={baseUrl + partner.image} alt={partner.name} width="150" />
+        <Media body="true" className="ml-5 mb-4">
           <Media heading>{partner.name}</Media>
           {partner.description}
         </Media>
@@ -25,6 +58,7 @@ function RenderPartner({ partner }) {
 }
 
 function About(props) {
+  /* moving to PartnerList functional component
   const partners = props.partners.map(partner => {
     //return <h5>{partner.name}</h5>;
     return (
@@ -33,7 +67,7 @@ function About(props) {
       </Media>
     );
   });
-
+*/
   return (
     <div className="container">
       <div className="row">
@@ -104,7 +138,8 @@ function About(props) {
           <h3>Community Partners</h3>
         </div>
         <div className="col mt-4">
-          <Media list>{partners}</Media>
+          {/*<Media list>{partners}</Media>*/}
+          <PartnerList partners={props.partners} />
         </div>
       </div>
     </div>
--- 3-React/nucampsite/src/redux/partners.js
+++ 3-React/nucampsite/src/redux/partners.js
@@ -1,8 +1,37 @@
-import { PARTNERS } from '../shared/partners';
+/* Prior code
+import { PARTNERS } from "../shared/partners";
 
 export const Partners = (state = PARTNERS, action) => {
-    switch (action.type) {
-        default:
-          return state;
-      }
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+*/
+
+// Workshop code
+import * as ActionTypes from "./ActionTypes";
+
+export const Partners = (
+  state = { isLoading: true, errMess: null, partners: [] },
+  action
+) => {
+  switch (action.type) {
+    case ActionTypes.ADD_PARTNERS:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: null,
+        partners: action.payload
+      };
+
+    case ActionTypes.PARTNERS_LOADING:
+      return { ...state, isLoading: true, errMess: null, partners: [] };
+
+    case ActionTypes.PARTNERS_FAILED:
+      return { ...state, isLoading: false, errMess: action.payload };
+
+    default:
+      return state;
+  }
 };
