8a7520159c8913b5240dc183a67f58844f81b8ad
arnoldporche <arnoldporche@yahoo.com>
2020-02-10 21:44:27 -0800

    Node Callback Hell and Promises

--- 5-NodeJS-Express-MongoDB/node-mongo/index.js
+++ 5-NodeJS-Express-MongoDB/node-mongo/index.js
@@ -4,7 +4,7 @@ const dboper = require("./operations");
 
 const url = "mongodb://localhost:27017/";
 const dbname = "nucampsite";
-
+/*
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
   assert.strictEqual(err, null);
 
@@ -54,3 +54,48 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
     );
   });
 });
+*/
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then(client => {
+    console.log("Connected correctly to server");
+    const db = client.db(dbname);
+
+    dboper
+      .insertDocument(
+        db,
+        { name: "Breadcrumb Trail Campground", description: "Test" },
+        "campsites"
+      )
+      .then(result => {
+        console.log("Insert Document:\n", result.ops);
+
+        return dboper.findDocuments(db, "campsites");
+      })
+      .then(docs => {
+        console.log("Found Documents:\n", docs);
+
+        return dboper.updateDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          { description: "Updated Test" },
+          "campsites"
+        );
+      })
+      .then(result => {
+        console.log("Updated Document:\n", result.result);
+
+        return dboper.findDocuments(db, "campsites");
+      })
+      .then(docs => {
+        console.log("Found Updated Documents:\n", docs);
+
+        return db.dropCollection("campsites");
+      })
+      .then(result => {
+        console.log("Dropped Collection:", result);
+
+        return client.close();
+      })
+      .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
--- 5-NodeJS-Express-MongoDB/node-mongo/operations.js
+++ 5-NodeJS-Express-MongoDB/node-mongo/operations.js
@@ -1,5 +1,5 @@
 const assert = require("assert").strict;
-
+/*
 exports.insertDocument = (db, document, collection, callback) => {
   const coll = db.collection(collection);
   coll.insertOne(document, (err, result) => {
@@ -30,4 +30,24 @@ exports.updateDocument = (db, document, update, collection, callback) => {
     assert.strictEqual(err, null);
     callback(result);
   });
+};
+*/
+exports.insertDocument = (db, document, collection) => {
+  const coll = db.collection(collection);
+  return coll.insertOne(document);
+};
+
+exports.findDocuments = (db, collection) => {
+  const coll = db.collection(collection);
+  return coll.find({}).toArray();
+};
+
+exports.removeDocument = (db, document, collection) => {
+  const coll = db.collection(collection);
+  return coll.deleteOne(document);
+};
+
+exports.updateDocument = (db, document, update, collection) => {
+  const coll = db.collection(collection);
+  return coll.updateOne(document, { $set: update }, null);
 };
