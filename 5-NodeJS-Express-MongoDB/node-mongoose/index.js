6a683ba8fba5a841d6d7f77ef8e72856dda07cd3
arnoldporche <arnoldporche@yahoo.com>
2020-02-14 21:34:31 -0800

    Mongoose Part 3

--- 5-NodeJS-Express-MongoDB/node-mongoose/index.js
+++ 5-NodeJS-Express-MongoDB/node-mongoose/index.js
@@ -4,37 +4,11 @@ const Campsite = require("./models/campsite");
 const url = "mongodb://localhost:27017/nucampsite";
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-/*
-connect.then(() => {
-  console.log("Connected correctly to server");
 
-  const newCampsite = new Campsite({
-    name: "React Lake Campground",
-    description: "test"
-  });
-
-  newCampsite
-    .save()
-    .then(campsite => {
-      console.log(campsite);
-      return Campsite.find();
-    })
-    .then(campsites => {
-      console.log(campsites);
-      return Campsite.deleteMany();
-    })
-    .then(() => {
-      return mongoose.connection.close();
-    })
-    .catch(err => {
-      console.log(err);
-      mongoose.connection.close();
-    });
-});
-*/
 connect.then(() => {
   console.log("Connected correctly to server");
 
@@ -44,10 +18,30 @@ connect.then(() => {
   })
     .then(campsite => {
       console.log(campsite);
-      return Campsite.find();
+
+      return Campsite.findByIdAndUpdate(
+        campsite._id,
+        {
+          $set: { description: "Updated Test Document" }
+        },
+        {
+          new: true
+        }
+      );
     })
-    .then(campsites => {
-      console.log(campsites);
+    .then(campsite => {
+      console.log(campsite);
+
+      campsite.comments.push({
+        rating: 5,
+        text: "What a magnificent view!",
+        author: "Tinus Lorvaldes"
+      });
+
+      return campsite.save();
+    })
+    .then(campsite => {
+      console.log(campsite);
       return Campsite.deleteMany();
     })
     .then(() => {
