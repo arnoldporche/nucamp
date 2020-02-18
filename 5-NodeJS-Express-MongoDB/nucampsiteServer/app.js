e578439d296ffccb894fa51182623b8833faff0b
arnoldporche <arnoldporche@yahoo.com>
2020-02-17 21:59:17 -0800

    Express Session Part 1

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -34,8 +36,57 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-09876-54321"));
+//app.use(cookieParser("12345-67890-09876-54321"));
 
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+  })
+);
+
+function auth(req, res, next) {
+  console.log(req.session);
+
+  if (!req.session.user) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      const err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+
+    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    const user = auth[0];
+    const pass = auth[1];
+    if (user === "admin" && pass === "password") {
+      req.session.user = "admin";
+      return next(); // authorized
+    } else {
+      const err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+  } else {
+    if (req.session.user === "admin") {
+      console.log("req.session:", req.session);
+      return next();
+    } else {
+      const err = new Error("You are not authenticated!");
+      err.status = 401;
+      return next(err);
+    }
+  }
+}
+
+/*
 function auth(req, res, next) {
   if (!req.signedCookies.user) {
     const authHeader = req.headers.authorization;
@@ -45,7 +96,9 @@ function auth(req, res, next) {
       err.status = 401;
       return next(err);
     }
-
+    console.log("authHeader:" + authHeader);
+    const buffet = Buffer.from("YWRtaW46cGFzc3dvcmQ=", "base64");
+    console.log("buffet:" + buffet);
     const auth = Buffer.from(authHeader.split(" ")[1], "base64")
       .toString()
       .split(":");
@@ -70,7 +123,7 @@ function auth(req, res, next) {
     }
   }
 }
-
+*/
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
